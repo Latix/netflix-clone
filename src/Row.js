ea73f1b0d2e52e14f54b38a2d725cd51cf48cf88
Kamsi Kodi <kamsikodi@gmail.com>
2021-03-23 14:19:41 +0100

    Code Refactored

--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import axios from './axios';
+import Youtube from 'react-youtube';
 import './Row.css';
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
-
+    const [trailerUrl, setTrailerUrl] = useState('');
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
@@ -16,6 +18,28 @@ function Row({ title, fetchUrl, isLargeRow }) {
         fetchData();
     }, [fetchUrl]);
 
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            
+            autoplay: 1,
+        }
+    }
+
+    const handleClick = (movie) => {
+        return;
+        if (trailerUrl){
+            setTrailerUrl('');
+        } else {
+            movieTrailer(movie?.name || "")
+            .then(url => {
+                //Https://www.youtube.com?v=12sdsd
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get('v'));
+            }).catch(err => console.log(err));
+        }
+    }
     return (
         <div className="row">
             <h2>{title}</h2>
@@ -23,11 +47,13 @@ function Row({ title, fetchUrl, isLargeRow }) {
                 {movies.map((movie) => (
                     <img 
                         className={`row__poster ${isLargeRow && "row__posterLarge"}`} 
+                        onClick={() => handleClick(movie)}
                         key={movie.id} 
                         src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                         alt={movie.name} />
                 ))}
             </div>  
+            {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
         </div>
     )
 }
