da5258dfe02bd370ff23a8ec455a9bb311a3ac4f
Kamsi Kodi <kamsikodi@gmail.com>
2021-03-23 10:49:19 +0100

    Condifured axios defaults

--- src/request.js
+++ src/request.js
@@ -8,5 +8,5 @@ export default {
     fetchComedyMovies: `/discover/movie?api_ke=${API_KEY}&with_genres=35`,
     fetchHorrorMovies: `/discover/movie?api_ke=${API_KEY}&with_genres=27`,
     fetchRomanceMovies: `/discover/movie?api_ke=${API_KEY}&with_genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_ke=${API_KEY}&with_genres=99`,
+    fetchDocumentaries: `/discover/movie?api_ke=${API_KEY}&with_genres=99 `,
 }
